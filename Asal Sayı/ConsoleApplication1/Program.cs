6cf4550e73c249d60971ce48e60a55abd24321eb
Sinan  Ganiz <sinan_ganiz@hotmail.com>
2019-11-18 00:58:37 +0300

    Tamam

---
+++ Asal Sayı/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir Sayı Giriniz=");
+            int sayac=0;
+            int sayı = int.Parse(Console.ReadLine());
+            for (int i = 2; i <sayı; i++)
+            {
+                if (sayı % i == 0)
+                {
+                    sayac++;
+                    Console.WriteLine("SAYI ASAL DEĞİLDİR");
+                    break;
+                }
+            }
+            if (sayac==0)
+            {
+                Console.Write("SAYI ASALDIR");
+            }
+             Console.ReadKey();
+        }
+    }
+}
